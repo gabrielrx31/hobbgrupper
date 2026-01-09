c1f4cc9835c6d84ba8c5ad4602d64b8d88067c4c
gabrielfiskbaek <gar001@edu.zealand.dk>
2026-01-09 13:11:33 +0100

    fixed validation

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const eventController = require('../controllers/eventController');
+const validateEvent = require('../middleware/eventValidation');
 
 const router = express.Router();
 
 router.get('/', eventController.getAllEvents);
 router.get('/:id', eventController.getEventById);
-router.post('/', eventController.createEvent);
+router.post('/', validateEvent, eventController.createEvent);
 router.put('/:id', eventController.updateEventById);
 router.delete('/:id', eventController.deleteEventById);
 
